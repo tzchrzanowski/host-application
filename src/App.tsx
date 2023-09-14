25af786ce1bd538667185fbad026ed149749a10c
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-14 15:51:47 +0800

    connected microfrontend1 to host-application and displayed it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 
+const Microfrontend1 = React.lazy(() => import("microfrontend1/App"));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Host Application
-      </header>
-    </div>
-  );
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          Host Application
+        </header>
+          <div className="microfrontends-container">
+              {Microfrontend1 && <Microfrontend1 />}
+
+          </div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/remoteTypes.d.ts
@@ -0,0 +1,7 @@
+///<reference types="react" />
+
+declare module "microfrontend1/App" {
+    const Microfrontend1: React.ComponentType;
+
+    export default Microfrontend1;
+}
